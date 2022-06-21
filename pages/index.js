02a188d66f4434c8c2c4cab26a8188c12eae4b3c
嶋田印刷 <107801411+shimada-print@users.noreply.github.com>
2022-06-21 13:12:55 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,7 @@ export default function Home() {
           <a href="https://nextjs.org">
 	  <img src="/next.js-logo.svg" alt="Next.jsのロゴ" width="350" height="250" /></a>
         </h1>
-        <div className={styles.pr1}>
-	<a href="https://note.com/shimada_print/n/n27449948782f" target="_blank">Shimada_Print</a> Project
-	</div>
+
         <div className={styles.info}>
 	このページはNext.jsのインストールが成功したら、
 	<br />表示できる少し改変したページです。
